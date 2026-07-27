64ce3c11843790b7e9b89be1e3148e156a26658b
Oliver Oayda <oaydaoliver@gmail.com>
2026-07-27 14:06:15 +1000

    add more default ts languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,9 +6,39 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   config = function()
+    vim.treesitter.language.register("javascript", "ojs")
+
+    local ensure_installed = {
+      "python",
+      "snakemake",
+      "lua",
+      "bash",
+      "javascript",
+      "markdown",
+      "markdown_inline",
+    }
+
+    if vim.fn.executable("tree-sitter") == 1 then
+      local generate_help = vim.fn.system({ "tree-sitter", "generate", "--help" })
+      if not generate_help:find("%-%-no%-bindings") then
+        require("nvim-treesitter.install").ts_generate_args = {
+          "generate",
+          "--abi",
+          tostring(vim.treesitter.language_version),
+        }
+      end
+      table.insert(ensure_installed, "latex")
+    end
+
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { "python", "lua", "bash" },
-      highlight = { enable = true, disable = { "latex" } },
+      ensure_installed = ensure_installed,
+      highlight = {
+        enable = true,
+        disable = function(lang, bufnr)
+          local ft = vim.bo[bufnr].filetype
+          return lang == "latex" and vim.tbl_contains({ "tex", "plaintex", "latex" }, ft)
+        end,
+      },
       indent = { enable = true, disable = { "latex" } },
 
       textobjects = {
